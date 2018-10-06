1a1267f250fd6f40f39f4a9dfa44903e7a30d432
clarkmcc <bmccauley4x@gmail.com>
2018-10-06 01:52:05 +0000

    Changed target

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var httpProxy = require('http-proxy');
-httpProxy.createServer(80, 'ppmproxy.herokuapp.com').listen(process.env.PORT || 8000, function() {
+httpProxy.createServer(80, 'www.google.com').listen(process.env.PORT || 8000, function() {
     console.log('Listening on port ' + process.env.PORT || 8000)
 });
