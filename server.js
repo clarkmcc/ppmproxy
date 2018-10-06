c9726e0f2ec54299ef069abd984635259c917985
clarkmcc <bmccauley4x@gmail.com>
2018-10-06 01:53:37 +0000

    Trying again

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
-var httpProxy = require('http-proxy');
-httpProxy.createServer(80, 'www.google.com').listen(process.env.PORT || 8000, function() {
-    console.log('Listening on port ' + process.env.PORT || 8000)
-});
+var http = require('http');
+
+http.createServer(onRequest).listen(3000);
+
+function onRequest(client_req, client_res) {
+  console.log('serve: ' + client_req.url);
+
+  var options = {
+    hostname: 'www.google.com',
+    port: 80,
+    path: client_req.url,
+    method: 'GET'
+  };
+
+  var proxy = http.request(options, function (res) {
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+
+  client_req.pipe(proxy, {
+    end: true
+  });
+}
